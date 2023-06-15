66963569bcf14a26790e4b133c6da79ef4116490
Sharon2408 <sharon2408@gmail.com>
2023-06-15 23:46:33 +0530

    ace

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'acecraft';
+  img='http://cdn.storehippo.com/s/5997cc7c4d6e8ffa20e50aae/ms.files/Home-1.jpg'
+  img1='https://d2pyicwmjx3wii.cloudfront.net/s/5997cc7c4d6e8ffa20e50aae/ms.settings/5256837ccc4abf1d39000001/webp/5a7061e0e20045f85a5e3301.png'
+}
