e2389da5e6a24a263a066cf5962f862d5804875d
Sharon2408 <sharon2408@gmail.com>
2023-06-23 23:07:35 +0530

    xss

--- src/app/enterprise/enterprise.component.ts
+++ src/app/enterprise/enterprise.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-enterprise',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class EnterpriseComponent {
 
+  @Input()
+  ngStyle!: { [klass: string]: any; };
+  ngDoCheck(){}
+
+ 
 }
